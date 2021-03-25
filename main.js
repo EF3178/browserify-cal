8e5a17e4495fd4ffa235cf1041edbbfe8dd196dc
EF3178 <64409068+EF3178@users.noreply.github.com>
2021-03-25 09:26:41 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const { google, outlook, office365 } = require("calendar-link");
+const ics = require('ics');
+let moment = require("moment");
+var base64 = require('base-64');
+console.log('hello');
